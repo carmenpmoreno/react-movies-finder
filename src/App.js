6b4e78e2eaa3accc06b866f648611365665119d1
carmenpmoreno <carmenp.moreno@trackandchain.com>
2020-09-13 11:37:07 +0200

    initial folders structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router';
+
+// import Home from './pages/Home';
+// import Detail from './pages/Detail';
+// import NotFound from './pages/NotFound';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        initial App
+        {/* <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/detail/:id" component={Detail} />
+          <Route component={NotFound} />
+        </Switch> */}
+      </div>
+    );
+  }
 }
 
 export default App;
