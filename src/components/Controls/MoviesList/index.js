7c75a529c43ffd8a3bb3cddb6d0693bceaaf27fe
VASS\carmen.morenoramos <carmen.morenoramos@vass.es>
2020-11-23 16:35:23 +0100

    sytles on movies cards

--- src/components/Controls/MoviesList/index.js
+++ src/components/Controls/MoviesList/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { setFavorite } from '../../helpers';
 import { useInput } from '../../customHooks';
@@ -16,51 +16,78 @@ const MoviesList = ( { movies, favoriteInfo, storeFavoriteOpinion, opinionOption
                     <div className="card-header">
                         <h3 className="card-title page-movie-title">{movie.Title}</h3>
                     </div>
-                    <div className="card-body">
+                    <div className="card-body page-movie-card-body">
                         <img
                             className="page-movie-image"
                             alt={movie.Title}
                             title={movie.Title}
                             src={movie.Poster}></img>
                     </div>
-                    <div className="card-footer page-movie-card-footer">
+                    <div className="card-footer">
                         {favoriteInfo === true
-                        ? <>
+                        ? <div className="page-movie-card-footer-buttons-wrapper">
                             <button
                                 type="button"
                                 className="btn btn-light page-movie-button"
                                 onClick={ () => setFavorite(movie)}
                                 ><i className="fas fa-star">Añadir</i>
                             </button>
-                        </>
-                        : ''
-                        }
-                        
-                        {opinionOptions === true && movie.opinion === undefined
-                        ? <>
-                            <input 
-                                placeholder="Escribe aquí tu opinión" 
-                                onChange={handleOnChange} />
                             <button
-                                onClick={() => storeFavoriteOpinion(value, movie)}
-                            >
-                                Guardar
-                            </button>
-                        </>
-                        : ''
-                        }
-                        {opinionOptions === true && movie.opinion !== undefined
-                        ? <p>Mis opinión: {movie.opinion}</p>
-                        : ''
-                        }
-                        <button
                             type="button"
                             className="btn btn-light page-movie-button"
                             >
-                            <Link to={`/detail/${movie.imdbID}`}>
-                                Saber más
-                            </Link>
-                        </button>
+                                <Link to={`/detail/${movie.imdbID}`}>
+                                    Saber más
+                                </Link>
+                            </button>
+                        </div>
+                        : ''
+                        }
+                        {opinionOptions === true && movie.opinion === undefined
+                        ? <div className="favorite-movie-card-footer">
+                            <input 
+                                className="page-card-input"
+                                placeholder="Escribe aquí tu opinión" 
+                                onChange={handleOnChange}
+                                maxLength="20"
+                            />
+                            <div className="page-movie-card-footer-buttons-wrapper">
+                                <button
+                                    type="button"
+                                    className="btn btn-light page-movie-button"
+                                    onClick={() => storeFavoriteOpinion(value, movie)}
+                                >
+                                    Guardar
+                                </button>
+                                <button
+                                type="button"
+                                className="btn btn-light page-movie-button"
+                                >
+                                    <Link to={`/detail/${movie.imdbID}`}>
+                                        Saber más
+                                    </Link>
+                                </button>
+                            </div>
+                            </div>
+                        : ''
+                        }
+                        {opinionOptions === true && movie.opinion !== undefined
+                        ? <div className="favorite-movie-card-footer">
+                            <p className="favorite-movie-card-footer-opinion-title">Mi opinión:</p>
+                            <p>{movie.opinion}</p>
+                            <div className="page-movie-card-footer-button-more-wrapper"> 
+                                <button
+                                type="button"
+                                className="btn btn-light page-movie-button"
+                                >
+                                    <Link to={`/detail/${movie.imdbID}`}>
+                                        Saber más
+                                    </Link>
+                                </button>
+                            </div>
+                        </div>
+                        : ''
+                        }
                         
                     </div>
                     
--- src/components/Controls/UserMenu/index.js
+++ src/components/Controls/UserMenu/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import './index.css';
 import { handleUserMenuButton, handleUserLogoutButton } from './helpers';
 import { userLogout } from '../../LoginPage/actions';
 
