e26fc70ad0127fe22cc2e76f1a1231c10880442d
VASS\carmen.morenoramos <carmen.morenoramos@vass.es>
2020-11-24 11:53:01 +0100

    get user name fron sessionStorage if exists on app refresh

--- src/App.js
+++ src/App.js
@@ -2,22 +2,46 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Layout from './components/Layout/index';
 import LoginPage from './components/LoginPage';
-import { Switch, Route } from 'react-router';
+import { push } from 'connected-react-router';
+
+import { useDispatch } from 'react-redux'
+import { LOGIN_ACTIONS } from './components/LoginPage/constants';
+
 
 import './styles/moviesFinder.scss';
 
 const App = ({ isLoadingUser }) => {
 
-  return isLoadingUser
-    ? <>
-    <Switch>
-        <Route 
-            exact path='/'
-            component={LoginPage} 
-        />
-      </Switch>
-    </>
-    : <Layout/>
+  const dispatch = useDispatch();
+  const userNameStored = sessionStorage.getItem('userName')
+  const checkUserLoaded = () => {
+
+    if (userNameStored !== null && userNameStored.length > 0 ) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  if ( checkUserLoaded() === false ) {
+    return <LoginPage/>
+  }
+
+  if( checkUserLoaded() === true ) {
+
+    dispatch(push('/home'))
+    dispatch({
+      type: LOGIN_ACTIONS.GET_USER_NAME,
+      user: userNameStored,
+    })  
+    dispatch({
+      type: LOGIN_ACTIONS.SET_USER_NAME_SUCESS,
+      isLoadingUser: false,
+    })            
+
+    return <Layout/>
+
+  }
 
 }
 
