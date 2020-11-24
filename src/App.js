a03ac51a6ce14010ad76bdd60073daf9a38fa2ef
VASS\carmen.morenoramos <carmen.morenoramos@vass.es>
2020-11-24 12:14:36 +0100

    refresh using redux sotre router location pathname

--- src/App.js
+++ src/App.js
@@ -1,60 +1,47 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import Layout from './components/Layout/index';
 import LoginPage from './components/LoginPage';
-import { push } from 'connected-react-router';
 
-import { useDispatch } from 'react-redux'
+import { push } from 'connected-react-router';
+import { useSelector, useDispatch } from 'react-redux'
 import { LOGIN_ACTIONS } from './components/LoginPage/constants';
 
 
 import './styles/moviesFinder.scss';
 
-const App = ({ isLoadingUser }) => {
+const App = () => {
 
-  const dispatch = useDispatch();
-  const userNameStored = sessionStorage.getItem('userName')
-  const checkUserLoaded = () => {
+    const dispatch = useDispatch(),
+      pathLocationName = useSelector(state => state.router.location.pathname),
+      userNameStored = sessionStorage.getItem('userName');
 
-    if (userNameStored !== null && userNameStored.length > 0 ) {
-      return true
+    const checkUserLoaded = userNameStored !== null && userNameStored.length > 0 ? true : false
+
+    if ( checkUserLoaded === false ) {
+
+        return <LoginPage/>
+
     } else {
-      return false
+
+        if( pathLocationName === '/') {
+          dispatch(push('/home'))
+        } else {
+          dispatch(push(pathLocationName))
+        }
+
+        dispatch({
+          type: LOGIN_ACTIONS.GET_USER_NAME,
+          user: userNameStored,
+        })  
+        dispatch({
+          type: LOGIN_ACTIONS.SET_USER_NAME_SUCESS,
+          isLoadingUser: false,
+        })            
+
+        return <Layout/>
+
     }
-  }
-
-  if ( checkUserLoaded() === false ) {
-    return <LoginPage/>
-  }
-
-  if( checkUserLoaded() === true ) {
-
-    dispatch(push('/home'))
-    dispatch({
-      type: LOGIN_ACTIONS.GET_USER_NAME,
-      user: userNameStored,
-    })  
-    dispatch({
-      type: LOGIN_ACTIONS.SET_USER_NAME_SUCESS,
-      isLoadingUser: false,
-    })            
-
-    return <Layout/>
-
-  }
 
 }
 
-const mapStateToProps = (state) => {
-  return {
-    isLoadingUser: state.LoginReducer.isLoadingUser
-  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    dispatch
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default App
