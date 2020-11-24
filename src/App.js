c970b6f336f77ca8fa8be3edc46a0cebb51f2a4c
VASS\carmen.morenoramos <carmen.morenoramos@vass.es>
2020-11-24 13:13:59 +0100

    improve app init when loaded user refresh layout

--- src/App.js
+++ src/App.js
@@ -2,33 +2,40 @@ import React from 'react';
 import Layout from './components/Layout/index';
 import LoginPage from './components/LoginPage';
 
+import { useSelector, useDispatch } from 'react-redux';
 import { push } from 'connected-react-router';
-import { useSelector, useDispatch } from 'react-redux'
 import { LOGIN_ACTIONS } from './components/LoginPage/constants';
-
 
 import './styles/moviesFinder.scss';
 
 const App = () => {
 
     const dispatch = useDispatch(),
+      isLoadingUser = useSelector(state => state.LoginReducer.isLoadingUser),
       pathLocationName = useSelector(state => state.router.location.pathname),
-      userNameStored = sessionStorage.getItem('userName');
+      userNameStored = sessionStorage.getItem('userName'),
+      checkUserLoaded = userNameStored !== null && userNameStored.length > 0 ? true : false;
 
-    const checkUserLoaded = userNameStored !== null && userNameStored.length > 0 ? true : false
-
-    if ( checkUserLoaded === false ) {
+    if ( isLoadingUser === true && checkUserLoaded === false ) {
 
         return <LoginPage/>
 
-    } else {
+    }
+    
+    if ( isLoadingUser === false ) {
 
+      return <Layout />
+
+    }
+
+    if ( isLoadingUser === true && checkUserLoaded === true ) {
+
+        //to refresh page and store when user refresh webApp
         if( pathLocationName === '/') {
           dispatch(push('/home'))
         } else {
-          dispatch(push(pathLocationName))
+            dispatch(push(pathLocationName))
         }
-
         dispatch({
           type: LOGIN_ACTIONS.GET_USER_NAME,
           user: userNameStored,
@@ -36,10 +43,9 @@ const App = () => {
         dispatch({
           type: LOGIN_ACTIONS.SET_USER_NAME_SUCESS,
           isLoadingUser: false,
-        })            
-
-        return <Layout/>
-
+        })  
+        
+        return <Layout />
     }
 
 }
